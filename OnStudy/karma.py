bca30ac6f3fdee325234f88d43e01570fa639850
jarquafelmu <thechaoselement@gmail.com>
2019-02-14 08:53:44 -0700

    karma system 1.0

--- OnStudy/karma.py
+++ OnStudy/karma.py
@@ -1,5 +1,7 @@
 from redbot.core import Config, commands, checks
+from redbot.core.utils.chat_formatting import error
 from redbot.core.utils.predicates import MessagePredicate
+from redbot.core.utils.embed import randomize_color
 from .logger import logger
 
 import discord
@@ -10,7 +12,10 @@ class Karma(commands.Cog):
     """
     thumbs_up_emoji = "\N{Thumbs Up Sign}"
     properties = {
-        "guild": None
+        "guild": None,
+        "channels": None,
+        "logic": None,
+        "karma_per_vote": 1
     }
     karma_roles = {
         "thanked": {
@@ -27,10 +32,11 @@ class Karma(commands.Cog):
         """Initialization function"""
         self.bot = bot
         self.properties["guild"] = args["guild"]
-        self.logic = args["logic"]
+        self.properties["channels"] = args["channels"]
+        self.properties["logic"] = args["logic"]
     
-        self.db = Config.get_conf(self, identifier=1742113358,
-                                  force_registration=True)
+        self.db = Config.get_conf(self, identifier=1742113358, force_registration=True)
+        
         default_member = {
             "been_thanked": {
                 "total": 0,
@@ -47,12 +53,22 @@ class Karma(commands.Cog):
     @commands.group(name="karma", aliases=["k", "Karma"])
     async def _karma(self, ctx):
         """
-        A group of commands for the karma cog
+        A group of commands for the karma cog. Leave blank to check your own karma.
         """
         pass
-
+        
     @_karma.command(name="check", aliases=["c"])
-    async def _karma_check(self, ctx, member: discord.Member):
+    async def _check_user_karma(self, ctx, member: discord.Member):
+        """
+        Displays a user's karma score.
+        """
+        karma_view = await self.build_karma_view(ctx, member)
+        if karma_view:
+            await ctx.send(embed=karma_view)
+        else:
+            await ctx.send(error("Member wasn't found."))
+
+    async def build_karma_view(self, ctx, member: discord.Member):
         """
         Displays a user's karma score.
         """
@@ -70,26 +86,92 @@ class Karma(commands.Cog):
                 await self.db.member(member).thanked_others()
             )
         except KeyError:
-            logger.error("Exception occured.")
+            logger.error("Member does not exist.")
+            return None
         else:
-            embed = discord.Embed(color=0xEE2222,
-                                  title=f"{member.name}'s karma")
-            embed.add_field(name="Thanked by others",
-                            value=been_thanked_val,
-                            inline=False)
-            embed.add_field(name="Thanked others",
-                            value=thanked_others_val,
-                            inline=False)
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=f"{member.display_name}'s karma")
+            embed.add_field(name="Thanked by others", value=been_thanked_val, inline=False)
+            embed.add_field(name="Thanked others", value=thanked_others_val, inline=False)
+            return randomize_color(embed)
 
-            await logger.info(
-                "{\n"
-                f"  member: {member.name}\n"
-                f"  {self.karma_roles['thanked']['name']}: {been_thanked_val}\n"
-                f"  {self.karma_roles['thanking']['name']}: {thanked_others_val}"
-                "\n}"
-            )
-        
+    @_karma.command(name="sync")
+    @checks.admin()
+    async def _karma_sync(self, ctx):
+        """
+        Syncs the karma scores for all users across the server. 
+        **WARNING**: This could take a long time. Please be patient.
+        """
+        # confirm this request
+        pred = await self.properties["logic"].confirm(ctx, msg=(
+            "This will reset all karma scores and then will process all of the messages on the server. "
+            "This process could take a while. **Are you sure you wish to continue?**"
+        ))
+        if pred is False:
+            return await ctx.send("Standing down.")
+        else:
+            await ctx.send("Beginning process.")
+            logger.debug("Syncing karma scores.")
+
+        # reset karma scores
+        await self.db.clear_all_members(ctx.guild)
+
+        async def status_msg(ctx, msg, *, msgObj = None):
+            if msgObj is None:
+                return await ctx.send(msg)
+            try:
+                await msgObj.edit(content=msg)
+            except discord.HTTPException:
+                logger.exception("Editing sync heartbeat message failed.")
+
+        # get the list of channels
+        channels = self.properties["guild"].text_channels
+        channels_processed = 0
+
+        # annouce how many channels are to be processed
+        channels_processed_msg = lambda current, total: f"Processed {current} of {total} channels."
+        processedMsgObj = await status_msg(ctx, channels_processed_msg(channels_processed, len(channels)))
+
+        latest_channel_msgObj = None
+        async with ctx.channel.typing():
+            # iterate through each channel:
+            for channel in channels:                
+                # iterate through the message history
+                async for message in channel.history():
+                    # process the reactions for the karma one
+                    payload = {}
+                    if message.author.bot or not self.properties["logic"].validate_member(message.author):
+                        continue
+                    payload["recipient"] = message.author                        
+                    for react in message.reactions:
+                        if (str(react.emoji) != self.thumbs_up_emoji):
+                            continue
+
+                        logger.debug("Found endorsement")
+
+                        async for user in react.users():
+                            if not self.properties["logic"].validate_member(user):
+                                continue
+
+                            # we have a user who is a valid member of the guild
+                            payload["sender"] = user
+
+                            # apply the normal rules for adding karma
+                            await self.process_reaction(payload=payload)
+
+                # announce channel is done
+                latest_channel = lambda channel: f"Processed channel: {channel}."
+                if latest_channel_msgObj is None:
+                    latest_channel_msgObj = await status_msg(ctx, latest_channel(channel.name))
+                else:                    
+                    await status_msg(ctx, latest_channel(channel.name), msgObj = latest_channel_msgObj)
+                logger.debug(latest_channel(channel.name))
+                
+                channels_processed = channels_processed + 1
+
+                # edit channel processed message to update the completed number
+                await status_msg(ctx, channels_processed_msg(channels_processed, len(channels)), msgObj=processedMsgObj)
+            await ctx.send("Done.")
+            logger.debug("Done.")
 
     @_karma.group(name="settings", aliases=["s", "set"])
     @checks.admin()
@@ -105,50 +187,53 @@ class Karma(commands.Cog):
         """
         Clears the karma data.
         """
+        logger.info("Resetting all member's karma scores!")
+
         # clears all the data stored for member of this guild in the database
         msg = (
             "Reset the karma for all members?"
             " There is **no** undo option!"
         )
-        if await self.logic.confirm(ctx, msg=msg):
+        if await self.properties["logic"].confirm(ctx, msg=msg):
             await self.db.clear_all_members(ctx.guild)
-        await ctx.send("Done.")
-        
+        await ctx.send("Done.")        
 
-    async def on_raw_reaction_add(self,
-                                  payload: discord.RawReactionActionEvent):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         """
         Member agrees to the rules.
         """
         await self.process_reaction(payload=payload, is_add_action=True)
         
 
-    async def on_raw_reaction_remove(self,
-                                     payload: discord.RawReactionActionEvent):
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         """
         Member no longer agrees to the rules.
         """
         await self.process_reaction(payload=payload, is_add_action=False)
         
 
-    async def process_reaction(self, *,
-                               payload: discord.RawReactionActionEvent,
-                               is_add_action: bool):
+    async def process_reaction(self, *, payload = None, is_add_action: bool = True):
         """
         Processes the reaction to see if it is a karma reaction.
         """
-        if str(payload.emoji) != self.thumbs_up_emoji:
-            return
+        if payload is None:            
+            return logger.error("payload is none!")
 
-        # get the guild from the payload
-        if self.properties["guild"] is None:
-            self.properties["guild"] = self.bot.get_guild(payload.guild_id)
+        if isinstance(payload, dict):
+            member_receiving = payload["recipient"]
+            member_giving = payload["sender"]
+        else:
+            # get the member from the guild using the user_id in the payload
+            member_giving = self.properties["guild"].get_member(payload.user_id)
+            channel = self.properties["guild"].get_channel(payload.channel_id)
+            message = await channel.get_message(payload.message_id)
+            member_receiving = message.author        
 
-        # get the member from the guild using the user_id in the payload
-        member_giving = self.properties["guild"].get_member(payload.user_id)
-        channel = self.properties["guild"].get_channel(payload.channel_id)
-        message = await channel.get_message(payload.message_id)
-        member_receiving = message.author
+            if str(payload.emoji) != self.thumbs_up_emoji:
+                return
+
+        # if (not self.properties["logic"].validate_member(member_receiving) or not self.properties["logic"].validate_member(member_receiving)):
+        #     return
 
         if member_giving.id == member_receiving.id:
             return logger.debug("member cannot give themselves karma.")
@@ -156,56 +241,42 @@ class Karma(commands.Cog):
         if member_receiving.bot:
             return logger.debug("bot may not receive karma.")
 
-        modifier = 1 if is_add_action else -1
-
-        await logger.debug(
-            f"{member_giving.name} "
-            f"{'gave a' if modifier == 1 else 'removed their'}"
-            f" karma "
-            f"{'to' if modifier == 1 else 'from'}"
-            f" {member_receiving.name}"
-        )
+        modifier = self.properties["karma_per_vote"] * 1 if is_add_action else -1
 
         await self.modify_karma(member_giving, member_receiving, modifier)
         
-
     async def modify_karma(self, member_giving, member_receiving, modifier):
         """
         Adds a karma point to both users
         """
-        await self.update_karma_category(member_giving,
-                                         self.karma_roles["thanking"]["name"],
-                                         modifier)
+        await self.update_karma_category(member_giving, self.karma_roles["thanking"]["name"], modifier)
 
-        await self.update_karma_category(member_receiving,
-                                         self.karma_roles["thanked"]["name"],
-                                         modifier)
-        
+        await self.update_karma_category(member_receiving, self.karma_roles["thanked"]["name"], modifier)
 
-    def to_non_negative(self, val: int):
+        msg = f"{member_giving.name} { 'added' if modifier > 0 else 'removed'} an endorsement { 'to' if modifier > 0 else ' from '} {member_receiving.name}"
+        await self.properties["channels"].log.send(msg)
+        logger.debug(msg)
+                
+    def nonnegative(self, val: int):
         """
         Returns either the `val` or zero.
         """
         return max(val, 0)
 
-    async def update_karma_category(self,
-                                    member: discord.Member,
-                                    category_id: str,
-                                    modifier: int):
+    async def update_karma_category(self, member: discord.Member, category_id: str, modifier: int):
         """
         Applies the modifier to the user's thanked score.
         """
         try:
             thank_category = await self.db.member(member).get_raw(category_id)
 
-            thank_category["total"] = self.to_non_negative(
+            thank_category["total"] = self.nonnegative(
                 thank_category["total"] + modifier
             )
-            thank_category["current"] = self.to_non_negative(
+            thank_category["current"] = self.nonnegative(
                 thank_category["current"] + modifier
             )
 
-            await self.db.member(member).set_raw(category_id,
-                                                 value=thank_category)
+            await self.db.member(member).set_raw(category_id, value=thank_category)
         except KeyError:
-            logger.exception("Exception occured.")
+            logger.exception("Member doesn't exist.")
