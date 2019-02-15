de7eaf4ee288b3e7f272b5fa5e7f90b05a709d64
jarquafelmu <thechaoselement@gmail.com>
2019-02-15 07:36:07 -0700

    formatting

--- OnStudy/oschannels.py
+++ OnStudy/oschannels.py
@@ -5,7 +5,7 @@ class OSChannels(commands.Cog):
     """
     Enum for channels to make it simplier to access them when needed
     """    
-    channel_ids = {        
+    __channel_ids = {        
         "courseList": 514518408122073116,
         "log": 485218362272645120,
         "newMembers": 484378858968055814,
@@ -16,46 +16,46 @@ class OSChannels(commands.Cog):
         self.bot = bot
         
         # channels
-        self.channel_courseList = None
-        self.channel_log = None
-        self.channel_newMembers = None
-        self.channel_welcome = None
-
+        self.__channel_courseList = None
+        self.__channel_log = None
+        self.__channel_newMembers = None
+        self.__channel_welcome = None
 
     @property
     def courseList(self):
         """
         Course List channel object
         """
-        if self.channel_courseList is None:
-            self.channel_courseList = self.bot.get_channel(self.channel_ids["courseList"])
-        return self.channel_courseList
+        if self.__channel_courseList is None:
+            self.__channel_courseList = self.bot.get_channel(self.__channel_ids["courseList"])
+        return self.__channel_courseList
 
     @property
     def log(self):
         """
         Logging channel object
         """
-        if self.channel_log is None:
-            self.channel_log = self.bot.get_channel(self.channel_ids["log"])
-        return self.channel_log
+        if self.__channel_log is None:
+            self.__channel_log = self.bot.get_channel(self.__channel_ids["log"])
+        return self.__channel_log
+
     @property
     def newMembers(self):
         """
         New Members channel object
         """
-        if self.channel_newMembers is None:
-            self.channel_newMembers = self.bot.get_channel(self.channel_ids["newMembers"])
-        return self.channel_newMembers
+        if self.__channel_newMembers is None:
+            self.__channel_newMembers = self.bot.get_channel(self.__channel_ids["newMembers"])
+        return self.__channel_newMembers
 
     @property
     def welcome(self):
         """
         Server Guidelines channel object
         """
-        if self.channel_welcome is None:
-            self.channel_welcome = self.bot.get_channel(self.channel_ids["welcome"])
-        return self.channel_welcome
+        if self.__channel_welcome is None:
+            self.__channel_welcome = self.bot.get_channel(self.__channel_ids["welcome"])
+        return self.__channel_welcome
 
     def anchor(self, channel):
         """
