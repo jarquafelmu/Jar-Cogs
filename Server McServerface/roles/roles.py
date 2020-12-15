525694f6d98bd58e545be634603f53b9f4111fb9
jarquafelmu <thechaoselement@gmail.com>
2020-12-15 16:49:56 -0700

    formatting

--- Server McServerface/roles/roles.py
+++ Server McServerface/roles/roles.py
@@ -4,21 +4,23 @@ from redbot.core.utils.chat_formatting import warning
 import discord
 import contextlib
 
+# TODO: update for rules role and the others
+# TODO: Create tool where a role can be assigned to a reaction emoji from bot commands
 
 class RoleManager(commands.Cog):
     """Manages roles for Server McServerface"""
-    
+
     guild_id = 493875452046475275
-    
+
     msg_rule_agreement_id = 508393348067885066
     msg_author_id = 513857600152928279
-    
+
     log_id = 509041710651670548
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.log = self.bot.get_channel(self.log_id)
-        
+
         self.roles = {
             "reader": {
                 "id": 506657944860098561
@@ -27,47 +29,47 @@ class RoleManager(commands.Cog):
                 "id": 506657837498368014
             }
         }
-        
+
         self.roles["reader"]["obj"] = self.bot.get_guild(self.guild_id).get_role(self.roles["reader"]["id"])
         self.roles["author"]["obj"] = self.bot.get_guild(self.guild_id).get_role(self.roles["author"]["id"])
-    
-    async def on_raw_reaction_add(self, payload):        
+
+    async def on_raw_reaction_add(self, payload):
         """
         Member agrees to the rules
-        """        
+        """
         await self.process_reaction(payload, True)
-                
-    async def on_raw_reaction_remove(self, payload):        
+
+    async def on_raw_reaction_remove(self, payload):
         """
         Member no longer agrees to the rules
-        """        
+        """
         await self.process_reaction(payload, False)
-                
+
     async def process_reaction(self, payload, add: bool):
         """
         Handles the processing of the reaction
         """
         member = self.bot.get_guild(self.guild_id).get_member(payload.user_id)
-        
+
         if member is None:
             return
-        
+
         print(payload.emoji)
-        
+
         emoji = str(payload.emoji)
-        
+
         print(emoji)
-        
+
         msg_id = payload.message_id
         if msg_id == self.msg_rule_agreement_id:
             await self.process_rule_agreement_reaction(member, emoji, add)
         elif msg_id == self.msg_author_id:
-            await self.process_author_reaction(member, emoji, add)        
-                
+            await self.process_author_reaction(member, emoji, add)
+
     async def process_rule_agreement_reaction(self, member: discord.Member, emoji: str, add: bool):
         """
         Handles the rule agreement reaction
-        """        
+        """
 
         if emoji.startswith("\N{THUMBS UP SIGN}"):
             if add:
@@ -85,7 +87,7 @@ class RoleManager(commands.Cog):
                 )
                 action = "removed"
                 await member.remove_roles(self.roles["reader"]["obj"])
-            
+
             await self.log.send(f"`{member.name}` {action} `reader` role.")
             with contextlib.suppress(discord.HTTPException):
                 # we don't want blocked DMs preventing the function working
