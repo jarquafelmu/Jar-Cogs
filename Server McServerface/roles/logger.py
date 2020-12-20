4e0f3844c9992dedd26c6e714c4f4954d7844f80
jarquafelmu <thechaoselement@gmail.com>
2020-12-19 22:32:53 -0700

    logger for server mcserverface

---
+++ Server McServerface/roles/logger.py
@@ -0,0 +1,32 @@
+import logging
+
+# create log with 'spam_application'
+logger = logging.getLogger("The Nook")
+logger.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s::%(funcName)s::%(lineno)d"
+    "- %(levelname)s - %(message)s"
+)
+
+# create console handler
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+consoleHandler.setFormatter(formatter)
+
+# allows to add only one instance of file handler and stream handler
+# if logger.handlers:
+#     for handler in logger.handlers:
+#         # add the handlers to the log
+#         # makes sure no duplicate handlers are added
+
+#         if not isinstance(handler, logging.StreamHandler):
+#             logger.addHandler(consoleHandler)
+#             print('added stream handler')
+# else:
+#     logger.addHandler(consoleHandler)
+#     print('added handler for the first time')
+
+# logger.debug('debug message')
+# logger.info('info message')
